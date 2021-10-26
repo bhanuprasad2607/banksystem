ed0509931183baed13a1b1a9638e5e1c90aff587
bhanuprasad2607 <bhanu12969097@gmail.com>
2021-10-26 22:53:27 +0530

    initial commit

--- src/customers/views.py
+++ src/customers/views.py
@@ -4,7 +4,6 @@ from .models import CustomersModel
 
 def customers(request):
     customer = CustomersModel.objects.all()
-    print(customer)
     context = {
         'customers': customer,
     }
